251012138b7f3498333a0f9ae92de291413a5757
runner <runner@Mac-1712737042122.local>
2024-04-10 08:28:11 +0000

    Updating lefthook to 1.6.9

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
 //        .binaryTarget(name: "lefthook", path: "lefthook.artifactbundle.zip"),
         .binaryTarget(
             name: "lefthook",
-            url: "https://github.com/csjones/lefthook-plugin/releases/download/1.6.8/lefthook.artifactbundle.zip",
-            checksum: "5a3016bb1d455296338887158c59ba1f85224a48c6ad567a526cbb7a3623086c"
+            url: "https://github.com/csjones/lefthook-plugin/releases/download/1.6.9/lefthook.artifactbundle.zip",
+            checksum: "b1f42bc748024cf2a94fd2a8a4461e8e45e9997870e0942739b67d05d00a66a1"
         ),
     ],
     swiftLanguageVersions: [.v5]
